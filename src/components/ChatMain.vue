3c0adfabf30ec74821ab994a1a7856fbae31bdf3
serhii93git <s.g.demchenko93@gmail.com>
2024-07-17 14:58:07 +0200

    add user message history

--- src/components/ChatMain.vue
+++ src/components/ChatMain.vue
@@ -4,6 +4,10 @@
       <h1>Chat Application</h1>
     </div>
     <div class="messages" ref="messagesContainer">
+      <!-- Перевіряємо наявність повідомлень та показуємо або історію повідомлень, або повідомлення про відсутність повідомлень -->
+      <div v-if="messages.length === 0 && isConnected" class="no-messages">
+        No messages yet. Be the first to send a message!
+      </div>
       <div v-for="(message, index) in messages" :key="index" class="message" :class="{'my-message': message.username === username, 'other-message': message.username !== username}">
         <div class="message-content">
           <strong>{{ message.username }}:</strong>
@@ -35,19 +39,42 @@ export default {
     };
   },
   methods: {
-
-
-
-    toggleTheme() {
-      document.body.classList.toggle('dark-theme');
+    async fetchMessageHistory() {
+      try {
+        const response = await fetch(`http://localhost:8000/messages/${this.username}`, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        if (response.ok) {
+          const history = await response.json();
+          this.messages = history.map(msg => ({
+            username: msg.username,  
+            content: msg.content,
+            send_time: msg.send_time
+          }));
+          this.$nextTick(() => {
+            // Автоматичне перемотування на останнє повідомлення
+            const container = this.$refs.messagesContainer;
+            container.scrollTop = container.scrollHeight;
+          });
+        } else {
+          console.error('Failed to fetch message history. Status:', response.status);
+          alert('Failed to fetch message history.');
+        }
+      } catch (error) {
+        console.error('Failed to fetch message history:', error);
+        alert('Failed to fetch message history.');
+      }
     },
-
-
-    connectWebSocket() {
+    async connectWebSocket() {
       if (this.username.trim() === "") {
         alert("Please enter a username.");
         return;
       }
+
+      await this.fetchMessageHistory(); // Fetch message history before connecting to WebSocket
+
       const wsUrl = `ws://localhost:8000/ws/chat?username=${this.username}`;
       this.websocket = new WebSocket(wsUrl);
 
@@ -76,7 +103,11 @@ export default {
         this.websocket.send(this.newMessage);
         this.newMessage = "";
       }
+    },
+    toggleTheme() {
+      document.body.classList.toggle('dark-theme');
     }
   }
 };
 </script>
+
